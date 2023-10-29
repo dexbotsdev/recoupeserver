88fe4dae9c82379faaabdd6fba3829f00829deb3
DefiCody <ambergridpro@gmail.com>
2023-10-29 17:30:13 +0530

    dev

--- src/index.ts
+++ src/index.ts
@@ -81,7 +81,7 @@ fastify.post('/processRecovery', recoveryOpts) ;
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 10000 });
+    await fastify.listen({ port: 5000 });
   } catch (err) {
     fastify.log.error(err); 
   }
