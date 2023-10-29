a7b8e1230a710f5cd3f4b0d052f770f980a4c100
DefiCody <ambergridpro@gmail.com>
2023-10-29 17:41:10 +0530

    w

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import "./loadEnv";
 
-import Fastify, { FastifyReply, FastifyRequest } from "fastify";
-import { clerkClient, clerkPlugin, getAuth } from "@clerk/fastify";
+import Fastify  from "fastify"; 
 import getContractMethodSignatures from "./services/EthersService";
 import { runRecoverERC } from "./services/FlashBotERC20Service";
 import { HttpStatusCode } from "axios";
@@ -10,19 +9,78 @@ const fastify = Fastify({ logger: true });
 const typeString = { type: 'string' }; // since i will be using this type a lot
 const typeNumber = { type: 'number' }; // since i will be using this type a lot
 
-/**
- * Register the Clerk plugin globally.
- * By default, Clerk will initialise using the API keys from the environment if found.
- *
- * If you prefer to pass the keys to the plugin explicitly, see `src/using-runtime-keys.ts`
- * If you prefer to register the plugin for specific routes only, see `src/authenticating-specific-routes.ts`
- */
-fastify.register(clerkPlugin);
+const port = process.env.PORT || 3000;
+const host = ("RENDER" in process.env) ? `0.0.0.0` : `localhost`;
+ 
+fastify.get('/', function (request, reply) {
+  reply.type('text/html').send(html)
+})
 
-fastify.get("/", async (req, reply) => {
-  reply.send('Hello There you are here ');
+fastify.listen({host: host, port: port }, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+})
 
-});
+const html = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hello from Render!</title>
+    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
+    <script>
+      setTimeout(() => {
+        confetti({
+          particleCount: 100,
+          spread: 70,
+          origin: { y: 0.6 },
+          disableForReducedMotion: true
+        });
+      }, 500);
+    </script>
+    <style>
+      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
+      @font-face {
+        font-family: "neo-sans";
+        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
+        font-style: normal;
+        font-weight: 700;
+      }
+      html {
+        font-family: neo-sans;
+        font-weight: 700;
+        font-size: calc(62rem / 16);
+      }
+      body {
+        background: white;
+      }
+      section {
+        border-radius: 1em;
+        padding: 1em;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin-right: -50%;
+        transform: translate(-50%, -50%);
+      }
+      section a {
+        text-decoration:none;
+        color: #1C151A;
+      }
+      section a:hover {
+        text-decoration:none;
+        color: #605A5C;
+      }
+    </style>
+  </head>
+  <body>
+    <section>
+      <a href="https://render.com/docs/deploy-node-fastify-app">Hello from Render using Fastify!</a>
+    </section>
+  </body>
+</html>
+`
 
 
 fastify.get('/contractDetails', async (request:any, reply)  => { 
@@ -77,14 +135,4 @@ fastify.post('/processRecovery', recoveryOpts) ;
             const erc20Recipient = values['erc20Recipient'];
             const ethBribeAmount = values['ethBribeAmount'].toString();
 */
-
-
-const start = async () => {
-  try {
-    await fastify.listen({ port: 5000 });
-  } catch (err) {
-    fastify.log.error(err); 
-  }
-};
-
-start();
+ 
